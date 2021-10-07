27f4666556c58808f8d77630cccdc4978b994b04
noveriojoee <noverio.joe.hasibuan@outlook.co.id>
2021-10-07 15:55:47 +0700

    update

--- lib/Components/ListViewTaskItem.dart
+++ lib/Components/ListViewTaskItem.dart
@@ -1,38 +1,47 @@
 import 'package:flutter/material.dart';
 
-class ListViewTaskItem extends StatefulWidget {
-  @override
-  State<ListViewTaskItem> createState() => _ListViewTaskItemState();
-}
+class ListViewTaskItem extends StatelessWidget {
+  bool _isChecked;
+  String _taskTitle;
+	Function checkboxCallback;
 
-class _ListViewTaskItemState extends State<ListViewTaskItem> {
-	bool _isChecked = false;
+	ListViewTaskItem(this._isChecked,this._taskTitle, this.checkboxCallback);
 
-	void onCheckboxChange(newValue) {
-		setState(() {
-			_isChecked = newValue;
-		});
-	}
-
-	@override
-	Widget build(BuildContext context) {
-		return ListTile(title: Text("this is the task", style: TextStyle(
-			decoration: _isChecked == true ? TextDecoration.lineThrough : null
-		),), trailing: TaskCheckBox(_isChecked,onCheckboxChange),);
-	}
-}
-
-class TaskCheckBox extends StatelessWidget {
-	final bool isChecked;
-
-	//This is awesome, we can pass in method or function as variable, instead of creating interface or protocol in ios
-	final Function checkboxCallback;
-
-	//What is this? constructor or what?
-	TaskCheckBox(this.isChecked, this.checkboxCallback);
+//  void onCheckboxChange(newValue) {
+//    setState(() {
+//      _isChecked = newValue;
+//    });
+//  }
 
   @override
   Widget build(BuildContext context) {
-    return Checkbox(onChanged:checkboxCallback, value: isChecked,);
+    return ListTile(
+      title: Text(
+				_taskTitle,
+        style: TextStyle(
+            decoration: _isChecked == true ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Checkbox(
+        value: _isChecked,
+        onChanged: checkboxCallback,
+        //onChanged: (newValue) => onCheckboxChange(newValue),
+      ),
+    );
   }
 }
+
+//There are sample for callback here..
+//class TaskCheckBox extends StatelessWidget {
+//	final bool isChecked;
+//
+//	//This is awesome, we can pass in method or function as variable, instead of creating interface or protocol in ios
+//	final Function checkboxCallback;
+//
+//	//What is this? constructor or what?
+//	TaskCheckBox(this.isChecked, this.checkboxCallback);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Checkbox(onChanged:checkboxCallback, value: isChecked,);
+//  }
+//}
--- lib/Components/ListViewWidget.dart
+++ lib/Components/ListViewWidget.dart
@@ -3,27 +3,41 @@ import 'package:what_to_do_app/Components/ListViewTaskItem.dart';
 import 'package:what_to_do_app/Models/TaskModel.dart';
 
 class ListViewWidget extends StatefulWidget {
-
   @override
   State<ListViewWidget> createState() => _ListViewWidgetState();
 }
 
 class _ListViewWidgetState extends State<ListViewWidget> {
-	List<TaskModel> models = [
-		TaskModel("xxx", false),
-		TaskModel("xxx", false),
-		TaskModel("xxx", false),
-		TaskModel("xxx", false)
-	];
+  List<TaskModel> models = [
+    TaskModel("Buying bread", false),
+    TaskModel("Buying papper", false),
+		TaskModel("Buying ciggrate", false)
+  ];
 
-	@override
-	Widget build(BuildContext context) {
-		return ListView(
-			children: <Widget>[
-				ListViewTaskItem(),
-				ListViewTaskItem(),
-				ListViewTaskItem()
-			],
-		);
-	}
+  void onTaskChecked(TaskModel model, bool newValue) {
+    setState(() {
+      model.toogleIsDone();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //Instead of doing this, we can doing this using liewViewBuilder
+//    return ListView(
+//      children: <Widget>[
+//        ListViewTaskItem(models[0].isDone, models[0].taskDescription, (newValue) => onTaskChecked(models[0],newValue)),
+//        ListViewTaskItem(models[1].isDone, models[1].taskDescription, (newValue) => onTaskChecked(models[1],newValue)),
+//        ListViewTaskItem(models[2].isDone, models[2].taskDescription, (newValue) => onTaskChecked(models[2],newValue))
+//      ],
+//    );
+
+    return ListView.builder(
+			itemBuilder: (context, index) {
+				return ListViewTaskItem(
+          models[index].isDone,
+          models[index].taskDescription,
+          (newValue) => onTaskChecked(models[index], newValue));
+    },
+		itemCount: models.length,);
+  }
 }
