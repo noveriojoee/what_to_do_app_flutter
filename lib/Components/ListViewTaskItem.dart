3a01b37c55d6b4ead032e883697eb58acf8a5b53
noveriojoee <noverio.joe.hasibuan@outlook.co.id>
2021-10-07 12:51:41 +0700

    update and adding callback with Function variable type

--- lib/Components/ListViewTaskItem.dart
+++ lib/Components/ListViewTaskItem.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
 
-class ListViewTaskItem extends StatelessWidget {
+class ListViewTaskItem extends StatefulWidget {
+  @override
+  State<ListViewTaskItem> createState() => _ListViewTaskItemState();
+}
+
+class _ListViewTaskItemState extends State<ListViewTaskItem> {
+	bool _isChecked = false;
+
+	void onCheckboxChange(newValue) {
+		setState(() {
+			_isChecked = newValue;
+		});
+	}
+
 	@override
 	Widget build(BuildContext context) {
-		return ListTile(title: Text("this is the task"), trailing: Checkbox(value: false,),);
+		return ListTile(title: Text("this is the task", style: TextStyle(
+			decoration: _isChecked == true ? TextDecoration.lineThrough : null
+		),), trailing: TaskCheckBox(_isChecked,onCheckboxChange),);
 	}
+}
+
+class TaskCheckBox extends StatelessWidget {
+	final bool isChecked;
+
+	//This is awesome, we can pass in method or function as variable, instead of creating interface or protocol in ios
+	final Function checkboxCallback;
+
+	//What is this? constructor or what?
+	TaskCheckBox(this.isChecked, this.checkboxCallback);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(onChanged:checkboxCallback, value: isChecked,);
+  }
 }
