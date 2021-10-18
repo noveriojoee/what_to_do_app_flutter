22036d900d6a5b8ef17a75c35eeb20a83c0f51c6
noveriojoee <noverio.joe.hasibuan@outlook.co.id>
2021-10-18 14:58:45 +0700

    update

--- lib/Screens/AddTaskScreen.dart
+++ lib/Screens/AddTaskScreen.dart
@@ -10,19 +10,61 @@ class AddTaskScreen extends StatefulWidget {
 class _AddTaskScreenState extends State<AddTaskScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      height: 1000,
-      width: 100,
-      child: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-          Text('Task Title' ,style: TextStyle(color: Colors.black, fontSize:20),),
-          TextField(),
-          Text('Task Description' ,style: TextStyle(color: Colors.black, fontSize: 20),),
-          TextField(),
-    ],),
-      ),);
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Task Title',
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                onChanged: (newValue) {
+                  print(newValue);
+                },
+              ),
+              Text(
+                'Task Description',
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                onChanged: (newValue) {
+                  print(newValue);
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RawMaterialButton(
+                  onPressed: () {
+                    print('button pressed');
+                  },
+                  child: Container(
+                    color: Colors.lightBlue,
+                    height: 50,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Add Task',
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
